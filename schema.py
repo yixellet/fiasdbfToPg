15cccc65f757e6b480092f52b0105e84deb9847c
yixellet <yixwellet@gmail.com>
2021-02-20 14:21:05 +0400

    Added new tables into schema file.

--- schema.py
+++ schema.py
@@ -210,5 +210,221 @@ schema = [
         "restrictions": ["not null"]
       }
     ]
-  }
+  },
+  {
+    "tablename": "centerst",
+    "fields": [
+      {
+        "name": "CENTERSTID",
+        "type": "smallint",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "NAME",
+        "type": "character varying(100)",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
+  {
+    "tablename": "currentst",
+    "fields": [
+      {
+        "name": "CURRENTSTID",
+        "type": "smallint",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "NAME",
+        "type": "character varying(100)",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
+  {
+    "tablename": "eststat",
+    "fields": [
+      {
+        "name": "ESTSTATID",
+        "type": "smallint",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "NAME",
+        "type": "character varying(20)",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "SHORTNAME",
+        "type": "character varying(20)",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
+  {
+    "tablename": "flattype",
+    "fields": [
+      {
+        "name": "FLTYPEID",
+        "type": "smallint",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "NAME",
+        "type": "character varying(20)",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "SHORTNAME",
+        "type": "character varying(20)",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
+  {
+    "tablename": "house",
+    "fields": [
+      {
+        "name": "AOGUID",
+        "type": "character varying(36)",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "REGIONCODE",
+        "type": "character varying(2)",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "POSTALCODE",
+        "type": "character varying(6)",
+        "restrictions": []
+      },
+      {
+        "name": "IFNSFL",
+        "type": "character varying(4)",
+        "restrictions": []
+      },
+      {
+        "name": "TERRIFNSFL",
+        "type": "character varying(4)",
+        "restrictions": []
+      },
+      {
+        "name": "IFNSUL",
+        "type": "character varying(4)",
+        "restrictions": []
+      },
+      {
+        "name": "TERRIFNSUL",
+        "type": "character varying(4)",
+        "restrictions": []
+      },
+      {
+        "name": "OKATO",
+        "type": "character varying(11)",
+        "restrictions": []
+      },
+      {
+        "name": "OKTMO",
+        "type": "character varying(11)",
+        "restrictions": []
+      },
+      {
+        "name": "UPDATEDATE",
+        "type": "date",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "HOUSENUM",
+        "type": "character varying(20)",
+        "restrictions": []
+      },
+      {
+        "name": "HOUSEGUID",
+        "type": "character varying(36)",
+        "restrictions": []
+      },
+      {
+        "name": "HOUSEID",
+        "type": "character varying(36)",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "CADNUM",
+        "type": "character varying(100)",
+        "restrictions": []
+      },
+      {
+        "name": "COUNTER",
+        "type": "integer",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "STATSTATUS",
+        "type": "integer",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "STRSTATUS",
+        "type": "integer",
+        "restrictions": []
+      },
+      {
+        "name": "BUILDNUM",
+        "type": "character varying(50)",
+        "restrictions": []
+      },
+      {
+        "name": "ESTSTATUS",
+        "type": "integer",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "STARTDATE",
+        "type": "date",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "ENDDATE",
+        "type": "date",
+        "restrictions": ["not null"]
+      },
+      {
+        "name": "NORMDOC",
+        "type": "character varying(36)",
+        "restrictions": []
+      },
+      {
+        "name": "STRUCNUM",
+        "type": "character varying(50)",
+        "restrictions": []
+      },
+      {
+        "name": "DIVTYPE",
+        "type": "smallint",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
+  {
+    "tablename": "ndoctype",
+    "fields": [
+      {
+        "name": "NDTYPEIDID",
+        "type": "integer",
+        "restrictions": ["not null"],
+        "pkey": True
+      },
+      {
+        "name": "NAME",
+        "type": "character varying(250)",
+        "restrictions": ["not null"]
+      }
+    ]
+  },
 ]
